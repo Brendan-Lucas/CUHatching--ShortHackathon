1adecf14c87680b0f639145ecd79b54f5ba1c814
mattias <matt1light@gmail.com>
2017-03-03 20:35:38 -0500

    add best learningstyles to students

--- Student.py
+++ Student.py
@@ -6,7 +6,8 @@ class Student():
         self.name = name
         self.age = age
         self.learningStyles = []
-        self.learnignStyles.append(LearningStyles(auditory, visual, kinetic, pace, social))
+        self.learnignStyles.append(LearningStyles(auditory, visual, kinetic, pace, social, 0))
+        self.bestLearningStyle
 
     def addAttributes(self, attributes):
         self.learningStyles.append(attributes)
@@ -16,3 +17,10 @@ class Student():
 
     def getAllAttributes(self):
         return self.learningStyles
+
+    def setBestLearningStyles(self):
+        bestLearningStyle = self.bestLearningStyle
+        for learningStyle in self.learningStyles:
+            if learningStyle.getTest() > bestLearningStyle.getTest():
+                bestLearningStyle = learningStyle
+        self.bestLearningStyle = bestLearningStyle
