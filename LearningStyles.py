fadd45c8b61b1b698d76f08708e4111cd1bd6e7f
Brendan Lucas <brendanwflucas@gmail.com>
2017-03-03 20:19:47 -0500

    add learningstyles container class

---
+++ LearningStyles.py
@@ -0,0 +1,10 @@
+
+
+class LearningStyles(self):
+
+  def __init__learningStyles(self, Auditory=50, Visual=50, Kinestetic=50, Pace=50, Social=50):
+      self.auditory = Auditory
+      self.visual = Visual
+      self.kinestetic = Kinestetic
+      self.pace = Pace
+      self.social = Social
